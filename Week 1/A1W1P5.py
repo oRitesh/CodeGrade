3b4e583aea85f4327c3d0e0083304d45fbb74b08
RiteshSR <1035730@hr.nl>
2024-09-11 12:12:48 +0200

    P5

---
+++ Week 1/A1W1P5.py
@@ -0,0 +1,10 @@
+number = input("Enter a 4 digit number:")
+
+d1 = int(number[0])
+d2 = int(number[1])
+d3 = int(number[2])
+d4 = int(number[3])
+
+total = d1 + d2 + d3 + d4
+
+print(f"{d1}+{d2}+{d3}+{d4}={total}")
