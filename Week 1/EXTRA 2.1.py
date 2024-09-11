9d2c3d647a7d4d34a94d5234540fa5fa3b115ced
RiteshSR <1035730@hr.nl>
2024-09-11 14:14:08 +0200

    Extra 1 en 2

---
+++ Week 1/EXTRA 2.1.py
@@ -0,0 +1,1 @@
+numbers = int(input("Enter 2 numbers"))
---
+++ Week 1/EXTRA 2.2.py
@@ -0,0 +1,10 @@
+in_seconds = int(input("Enter the amount of seconds:"))
+
+# Calculate hours, minutes, and remaining seconds
+hours = in_seconds // 3600  # 1 hour = 3600 seconds
+remaining_seconds = in_seconds % 3600 # Calculates the remaining second by using a % which devides and then gives the remains as an answer
+minutes = remaining_seconds // 60  # 1 minute = 60 seconds
+seconds = remaining_seconds % 60
+
+# Output the result as whole numbers, .02d so that the seconds and minutes have a 0 in front when its a single number
+print(f"{hours}:{minutes:02d}:{seconds:02d}")
